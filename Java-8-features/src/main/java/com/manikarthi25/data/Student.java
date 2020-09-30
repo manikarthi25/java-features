4204d91f5e2430abd3595acf7fbd2bc1e7cda5df
mani <manikarthi25@gmail.com>
2020-09-30 21:54:55 +0530

    Consumer - Method reference

--- Java-8-features/src/main/java/com/manikarthi25/data/Student.java
+++ Java-8-features/src/main/java/com/manikarthi25/data/Student.java
@@ -58,6 +58,10 @@ public class Student {
 	public void setActivities(List<String> activities) {
 		this.activities = activities;
 	}
+	
+	public void printListOfActivities() {
+		System.out.println(activities);
+	}
 
 	@Override
 	public String toString() {
