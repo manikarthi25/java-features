91264f3da0c339ca69da0902f6ceb233efc8a5d3
mani <manikarthi25@gmail.com>
2020-09-30 16:35:02 +0530

    Function Interface

---
+++ Java-8-features/src/main/java/com/manikarthi25/functioninalterface/FuncationExample.java
@@ -0,0 +1,19 @@
+package com.manikarthi25.functioninalterface;
+
+import java.util.function.Function;
+
+public class FuncationExample {
+
+	static Function<String, String> f1 = (name) -> name.toUpperCase();
+
+	static Function<String, String> f2 = (name) -> name.concat("default");
+
+	public static void main(String a[]) {
+
+		System.out.println(f1.apply("java8"));
+		System.out.println(f1.andThen(f2).apply("java8")); // first f1 then f2
+		System.out.println(f1.compose(f2).apply("java8")); // first f2 then f1
+
+	}
+
+}
