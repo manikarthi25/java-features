dc249953f144bdfab4518401d592b24aa075357d
mani <manikarthi25@gmail.com>
2020-10-01 14:37:52 +0530

    Sort, distinct and count

--- Java-8-features/src/main/java/com/manikarthi25/stream/StreamFlatMapExample.java
+++ Java-8-features/src/main/java/com/manikarthi25/stream/StreamFlatMapExample.java
@@ -17,8 +17,45 @@ public class StreamFlatMapExample {
 		return studentActivities;
 	}
 
+	public static List<String> getStudentActivitiesDistinct() {
+		List<String> studentActivities = StudentDataBase.getStudentDetails().stream() // Stream<Student>
+				.map(Student::getActivities) // Stream<List<String>>
+				.flatMap(List::stream) // Stream<String>
+				.distinct() // Stream<String> - with distinct
+				.collect(Collectors.toList());
+
+		return studentActivities;
+	}
+	
+	public static List<String> getStudentActivitiesDistinctSort() {
+		List<String> studentActivities = StudentDataBase.getStudentDetails().stream() // Stream<Student>
+				.map(Student::getActivities) // Stream<List<String>>
+				.flatMap(List::stream) // Stream<String>
+				.map(String::toUpperCase)
+				.distinct() // Stream<String> - with distinct
+				.sorted() // Stream<String> - sorted // based on ascii value it will be sorted
+				.collect(Collectors.toList());
+
+		return studentActivities;
+	}
+
+	public static long getStudentActivitiesCount() {
+		long noOfStudentActivities = StudentDataBase.getStudentDetails().stream() // Stream<Student>
+				.map(Student::getActivities) // Stream<List<String>>
+				.flatMap(List::stream) // Stream<String>
+				.distinct() // Stream<String> - with distinct
+				.count();
+
+		return noOfStudentActivities;
+	}
+
 	public static void main(String[] args) {
-		System.out.println(getStudentActivities());
+
+		System.out.println("Without Distinct = " + getStudentActivities());
+		System.out.println("With Distinct = " + getStudentActivitiesDistinct());
+		System.out.println("With Distinct and Sorted = " + getStudentActivitiesDistinctSort());
+		System.out.println("With Distinct and Count = " + getStudentActivitiesCount());
+		
 	}
 
 }
