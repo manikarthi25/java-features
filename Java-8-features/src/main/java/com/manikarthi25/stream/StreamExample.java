f62f29d8edfe0aed482eb18d2c7570023579abbc
mani <manikarthi25@gmail.com>
2020-10-01 11:57:26 +0530

    Stream with predicate and without predicate

---
+++ Java-8-features/src/main/java/com/manikarthi25/stream/StreamExample.java
@@ -0,0 +1,34 @@
+package com.manikarthi25.stream;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import com.manikarthi25.data.Student;
+import com.manikarthi25.data.StudentDataBase;
+
+public class StreamExample {
+
+	public static void main(String a[]) {
+
+		// without predicate
+		Map<String, List<String>> studentMapWithoutPredicate = StudentDataBase.getStudentDetails().stream()
+				.filter(student -> student.getGradeLevel() >= 3).filter(student -> student.getGpa() >= 6)
+				.collect(Collectors.toMap(Student::getName, Student::getActivities));
+
+		System.out.println("without predicate : " + studentMapWithoutPredicate);
+
+		// with predicate
+		Predicate<Student> predicateGradeLevel = (student) -> student.getGradeLevel() >= 3;
+		Predicate<Student> predicateGpa = (student) -> student.getGpa() >= 6;
+
+		Map<String, List<String>> studentMapWithPredicate = StudentDataBase.getStudentDetails().stream()
+				.filter(predicateGradeLevel).filter(predicateGpa)
+				.collect(Collectors.toMap(Student::getName, Student::getActivities));
+
+		System.out.println("With Predicate : " + studentMapWithPredicate);
+
+	}
+
+}
