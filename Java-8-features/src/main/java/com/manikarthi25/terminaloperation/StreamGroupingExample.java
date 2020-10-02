f995e7daa311805a312b84ad82ead52e71a397a0
mani <manikarthi25@gmail.com>
2020-10-02 13:33:39 +0530

    Grouping updated

--- Java-8-features/src/main/java/com/manikarthi25/terminaloperation/StreamGroupingExample.java
+++ Java-8-features/src/main/java/com/manikarthi25/terminaloperation/StreamGroupingExample.java
@@ -1,11 +1,11 @@
 package com.manikarthi25.terminaloperation;
 
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
-
-import javax.xml.bind.SchemaOutputResolver;
 
 import com.manikarthi25.data.Student;
 import com.manikarthi25.data.StudentDataBase;
@@ -34,30 +34,57 @@ public class StreamGroupingExample {
 
 	public static void twoLevelGrouping_2() {
 		Map<String, Integer> studentNotebookCount = StudentDataBase.getStudentDetails().stream()
-						.collect(Collectors.groupingBy(Student::getName, Collectors.summingInt(Student::getNoteBooks)));
+				.collect(Collectors.groupingBy(Student::getName, Collectors.summingInt(Student::getNoteBooks)));
 		System.out.println();
 		System.out.println("Grouping and summing :" + studentNotebookCount);
+
+	}
+
+	public static void threeArgumentGroupingBy() {
+		HashMap<String, List<Student>> studentHashMap = StudentDataBase.getStudentDetails().stream()
+				.collect(Collectors.groupingBy(Student::getName, HashMap::new, Collectors.toList()));
+		System.out.println();
+		System.out.println("Three Arguments : " + studentHashMap);
+		System.out.println();
+		System.out.println("Kannan Details Only : " + studentHashMap.get("kannan"));
+	}
+
+	public static void findMaxGpaGroupingByGradeLevel() {
+		Map<Integer, Optional<Student>> studentMapOptional = StudentDataBase.getStudentDetails().stream().collect(
+				Collectors.groupingBy(Student::getGradeLevel, Collectors.maxBy(Comparator.comparing(Student::getGpa))));
+		System.out.println();
+		System.out.println("Find maximum GPA group bt grade level  - with optional : " + studentMapOptional);
+		
+		Map<Integer, Student> studentMap = StudentDataBase.getStudentDetails().stream().collect(Collectors.groupingBy(Student::getGradeLevel,
+				Collectors.collectingAndThen(Collectors.maxBy(Comparator.comparing(Student::getGpa)), Optional::get)));
+		
+		System.out.println();
+		System.out.println("Find maximum GPA group by grade level  - without optional : " + studentMap);
 	}
 	
-	public static void threeArgumentGroupingBy() {
-			HashMap<String, List<Student>> studentHashMap = StudentDataBase.getStudentDetails().stream()
-					.collect(Collectors.groupingBy(Student::getName, HashMap::new, Collectors.toList()));
-			System.out.println();
-			System.out.println("Three Arguments : " + studentHashMap);
-			System.out.println();
-			System.out.println("Kannan Details Only : " + studentHashMap.get("kannan"));
+	public static void findMinGpaGroupingByGradeLevel() {
+		Map<Integer, Optional<Student>> studentMapOptional = StudentDataBase.getStudentDetails().stream().collect(
+				Collectors.groupingBy(Student::getGradeLevel, Collectors.minBy(Comparator.comparing(Student::getGpa))));
+		System.out.println();
+		System.out.println("Find minimum GPA group by grade level  - with optional : " + studentMapOptional);
+		
+		Map<Integer, Student> studentMap = StudentDataBase.getStudentDetails().stream().collect(Collectors.groupingBy(Student::getGradeLevel,
+				Collectors.collectingAndThen(Collectors.minBy(Comparator.comparing(Student::getGpa)), Optional::get)));
+		
+		System.out.println();
+		System.out.println("Find minimum GPA group by grade level  - without optional : " + studentMap);
 	}
 
 	public static void main(String[] args) {
-		
+
 		groupingStudentByGender();
 		customiziedGroupingStudent();
 		twoLevelGrouping_1();
 		twoLevelGrouping_2();
 		threeArgumentGroupingBy();
+		findMaxGpaGroupingByGradeLevel();
+		findMinGpaGroupingByGradeLevel();
 
 	}
 
 }
-
-
