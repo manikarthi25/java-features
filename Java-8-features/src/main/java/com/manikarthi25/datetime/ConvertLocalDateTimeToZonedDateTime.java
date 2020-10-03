0e6806bd17fdc6ad54891813a4e7faf7d480d38b
mani <manikarthi25@gmail.com>
2020-10-03 19:34:24 +0530

    Convert ZonedDateTime and LocalDateTime

---
+++ Java-8-features/src/main/java/com/manikarthi25/datetime/ConvertLocalDateTimeToZonedDateTime.java
@@ -0,0 +1,30 @@
+package com.manikarthi25.datetime;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+
+public class ConvertLocalDateTimeToZonedDateTime {
+
+	public static void main(String[] args) {
+		
+		// Convert LocalDateTime to ZonedDateTime
+		LocalDateTime localDateTimeWithoutZone = LocalDateTime.now();
+		System.out.println("localDateTime with out zone  :" + localDateTimeWithoutZone);
+		
+		ZonedDateTime zonedDateTimeimeLocal = localDateTimeWithoutZone.atZone(ZoneId.of("America/Chicago"));
+		System.out.println("localDateTime with zone      :" + zonedDateTimeimeLocal);
+		Instant inatant  = Instant.now();
+		
+		ZonedDateTime zonedDateTimeimeInstant = inatant.atZone(ZoneId.of("America/Chicago"));
+		System.out.println("Instant with zone            :" + zonedDateTimeimeInstant);
+		
+		OffsetDateTime localDateTimeOffset = LocalDateTime.now().atOffset(ZoneOffset.ofHours(-6));
+		System.out.println("Add offset with LocalDateTime :" + localDateTimeOffset);
+
+	}
+
+}
